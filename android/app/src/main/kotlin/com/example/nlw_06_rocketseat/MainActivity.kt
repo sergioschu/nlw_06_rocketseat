a8c4cd8dd3dc5e11d28a2d374548aeb87e3644c1
Sergio Jonas Schulz <sergioschu@gmail.com>
2021-06-22 12:21:25 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/nlw_06_rocketseat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nlw_06_rocketseat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'modules/home/home_page.dart';
+import 'modules/login/login_page.dart';
+import 'modules/splash/splash_page.dart';
+import 'shared/themes/app_colors.dart';
+
+void main() {
+  runApp(AppWidget());
+}
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pay Flow',
+      theme: ThemeData(
+        primaryColor: AppColors.primary,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
